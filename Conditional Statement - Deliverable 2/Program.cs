7cbf55ad048ce36077897421c0bbab8e12888b05
zhenw <zhenw@DESKTOP-A960KRD>
2020-01-30 22:37:05 -0500

    Changed grading scale according to the course syllabus

--- Conditional Statement - Deliverable 2/Program.cs
+++ Conditional Statement - Deliverable 2/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ Author: Zhenwei Li
+ Date: 1/27/2019
+ Comments: This C# Console application code demonstrates the use of 
+           conditional statements after getting input from users
+*/
+using System;
 
 namespace Conditional_Statement___Deliverable_2
 {
@@ -23,18 +29,42 @@ namespace Conditional_Statement___Deliverable_2
                     Console.WriteLine("Value entered cannot be greater than 100 or smaller than 0. Please exit and try again");
 
                 // If grade is between 90-100, display message
-                if (grade >= 90 && grade <= 100 )
+                if (grade >= 98 && grade <= 100 )
+                    Console.WriteLine("Your letter grade is: A+");
+                // If grade is between 80-90, display message
+                else if (grade >= 92 && grade < 98)
                     Console.WriteLine("Your letter grade is: A");
                 // If grade is between 80-90, display message
-                else if (grade >= 80 && grade < 90)
+                else if (grade >= 90 && grade < 92)
+                    Console.WriteLine("Your letter grade is: A-");
+                // If grade is between 80-90, display message
+                else if (grade >= 88 && grade < 90)
+                    Console.WriteLine("Your letter grade is: B+");
+                // If grade is between 80-90, display message
+                else if (grade >= 82 && grade < 88)
                     Console.WriteLine("Your letter grade is: B");
-                // If grade is between 70-80, display message
-                else if (grade >= 70 && grade < 80)
+                // If grade is between 80-90, display message
+                else if (grade >= 80 && grade < 82)
+                    Console.WriteLine("Your letter grade is: B-");
+                // If grade is between 80-90, display message
+                else if (grade >= 78 && grade < 80)
+                    Console.WriteLine("Your letter grade is: C+");
+                // If grade is between 80-90, display message
+                else if (grade >= 72 && grade < 78)
                     Console.WriteLine("Your letter grade is: C");
-                // If grade is between 60-70, display message
-                else if (grade >= 60 && grade < 70)
-                // If grade below 60, display message
+                // If grade is between 80-90, display message
+                else if (grade >= 70 && grade < 72)
+                    Console.WriteLine("Your letter grade is: C-");
+                // If grade is between 80-90, display message
+                else if (grade >= 68 && grade < 70)
+                    Console.WriteLine("Your letter grade is: D+");
+                // If grade is between 80-90, display message
+                else if (grade >= 62 && grade < 68)
                     Console.WriteLine("Your letter grade is: D");
+                // If grade is between 70-80, display message
+                else if (grade >= 60 && grade < 62)
+                    Console.WriteLine("Your letter grade is: D-");
+                // If gradeis between 60-70, display message
                 else if (grade < 60 && grade >= 0)
                     Console.WriteLine("Your letter grade is: F");
 
